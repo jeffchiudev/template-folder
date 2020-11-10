eae8d0acedc7e4749975f067d3faf6c05841c87d
Jeff Chiu <jwchiu82@gmail.com>
2020-11-10 09:53:42 -0800

    reorganize template folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/js/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'NAME HERE',
+      title: 'TODO',
       template: './src/index.html',
       inject: 'body'
     })
