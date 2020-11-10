6bd1cf08ac8c9944b64bdfda77a0fc4e2eaf9b4d
Jeff Chiu <jwchiu82@gmail.com>
2020-11-10 09:22:15 -0800

    update folder

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'NAME HERE',
       template: './src/index.html',
       inject: 'body'
     })
